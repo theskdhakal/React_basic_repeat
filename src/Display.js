5fcfd3f0b53a223a701e039b804b7e8d9cdec5f3
Shiva Dhakal <theskdhakal@gmail.com>
2023-02-20 13:13:44 +1100

    updated

--- src/Display.js
+++ src/Display.js
@@ -1,3 +1,24 @@
+// export const Display = ({ submit }) => {
+
+//   return (<div>{submit}</div>);
+// };
+
+import { useState, useEffect } from "react";
+
 export const Display = ({ submit }) => {
-  return <div>{submit}</div>;
+  const [previousSubmits, setPreviousSubmits] = useState([]);
+
+  useEffect(() => {
+    if (submit !== "") {
+      setPreviousSubmits((prev) => [...prev, submit]);
+    }
+  }, [submit]);
+
+  return (
+    <div>
+      {previousSubmits.map((submit, index) => (
+        <p>{submit}</p>
+      ))}
+    </div>
+  );
 };
